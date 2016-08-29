0a71a93d401d16d2833fdb5b6101f8d664d76f5a
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2016-08-29 22:02:57 +0800

    init commit

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("testLog", "this is a log");
+        Toast.makeText(MainActivity.this,
+                "this is a toast",
+                Toast.LENGTH_SHORT).show();
+
+    }
+}
